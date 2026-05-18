27f9b9786ecbd7fad6a82bc64752c482ee2f0f7b
Ivan Bueno Garrote <ibuegar@posgrado.upv.es>
2026-05-18 21:26:03 +0200

    ibuegar: Latitud y longitud del marcador

--- lib/presentation/providers/charta_provider.dart
+++ lib/presentation/providers/charta_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 
 final formNomenProvider = StateProvider<String>((ref) => '');
@@ -7,3 +8,6 @@ final formNomenProvider = StateProvider<String>((ref) => '');
 final formColorProvider = StateProvider<Color>((ref) => Colors.red);
 
 final markerPositumProvider = StateProvider<bool>((ref) => false);
+
+final Position initialisMarkerPositio = Position(-122.467895, 37.800126);
+final coordsMarkerProvider = StateProvider<Position>((ref) => initialisMarkerPositio);
--- lib/presentation/screens/charta/charta_screen.dart
+++ lib/presentation/screens/charta/charta_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
+import '../../widgets/widgets.dart';
+
 
 class ChartaScreen extends ConsumerStatefulWidget {
   const ChartaScreen({super.key});
@@ -16,17 +18,40 @@ class _ChartaScreenState extends ConsumerState<ChartaScreen> {
 
   CircleAnnotationManager? _circleAnnotationManager;
 
+  Cancelable? _dragCancelable;
+
   void _initiareCircleAnnotations(MapboxMap mapboxmap) {
 
     mapboxmap.annotations.createCircleAnnotationManager().then((manager) {
       _circleAnnotationManager = manager;
+
+      _setupDragListener(manager);
+
       _addereVelRenovaMarker();
     });
+  }
+
+  void _setupDragListener(CircleAnnotationManager manager) {
+
+    _dragCancelable?.cancel();
+
+    _dragCancelable = manager.dragEvents(
+      onChanged: (CircleAnnotation annotation) {
+        final pos = annotation.geometry.coordinates;
+        ref.read(coordsMarkerProvider.notifier).state = pos;
+      },
+      onEnd: (CircleAnnotation annotation) {
+        final pos = annotation.geometry.coordinates;
+        ref.read(coordsMarkerProvider.notifier).state = pos;
+      }
+    );
   }
 
   Future<void> _addereVelRenovaMarker() async {
     final manager = _circleAnnotationManager;
     if(manager == null) return;
+
+    await manager.deleteAll();
 
     final placed = ref.read(markerPositumProvider);
 
@@ -35,7 +60,7 @@ class _ChartaScreenState extends ConsumerState<ChartaScreen> {
       return;
     }
 
-    final situs = Position(-122.467895, 37.800126);
+    final situs = ref.read(coordsMarkerProvider);
 
     final color = ref.read(formColorProvider);
 
@@ -55,6 +80,14 @@ class _ChartaScreenState extends ConsumerState<ChartaScreen> {
   }
 
   @override
+  void dispose() {
+    _dragCancelable?.cancel();
+    super.dispose();
+  }
+
+
+
+  @override
   Widget build(BuildContext context) {
 
     ref.listen<bool>(markerPositumProvider, (prev, next) {
@@ -72,20 +105,25 @@ class _ChartaScreenState extends ConsumerState<ChartaScreen> {
             key: const ValueKey('main_map'),
             cameraOptions: CameraOptions(
               center: Point(
-                coordinates: Position(-122.467895, 37.800126),
+                coordinates: initialisMarkerPositio,
               ),
               zoom: 14.5,
             ),
             styleUri: MapboxStyles.MAPBOX_STREETS,
             onMapCreated: _initiareCircleAnnotations,
           ),
-          const Align(
+          Align(
             alignment: Alignment.topRight,
             child: Padding(
               padding: EdgeInsets.all(12.0),
-              child: ComplereForm()
-            ),
-          )
+                child: ref.watch(markerPositumProvider) 
+                  ? InformaUsoris(
+                    nomen: ref.watch(formNomenProvider),
+                    positio: ref.watch(coordsMarkerProvider),
+                    color: ref.watch(formColorProvider)
+                  ) : ComplereForm(),
+              ),
+          ),
         ]
       )
     );
