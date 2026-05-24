642d522c3752d8dbd76e7cb9804e9dc013592de4
Ivan Bueno Garrote <ibuegar@posgrado.upv.es>
2026-05-24 22:31:21 +0200

    ibuegar: Petición http y mapeado de la respuesta

---
+++ lib/mappers/pokemon_mapper.dart
@@ -0,0 +1,21 @@
+import 'package:flu_avm/config/config.dart';
+import 'package:flu_avm/models/pokeapi_responsio.dart';
+
+
+class PokemonMapper {
+
+  static Pokemon pokeApiPokemonToEntity( Map<String, dynamic> json) {
+
+    final pokeApiPokemon = PokeApiPokemonResponsio.fromJson(json);
+
+    return Pokemon(
+      id: pokeApiPokemon.id,
+      nomen: pokeApiPokemon.name,
+      altitudo: pokeApiPokemon.height,
+      pondus: pokeApiPokemon.weight,
+      facultates: pokeApiPokemon.abilities.map((facultas) => facultas.ability!.name).toList(),
+      faciemImaginem: pokeApiPokemon.sprites.other?.home.frontDefault,
+      
+    );
+  }
+}
--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flu_avm/mappers/pokemon_mapper.dart';
 
 class PokemonService {
   static getPokemon<String>(String pokemonId) async {
@@ -7,9 +8,14 @@ class PokemonService {
 
     try {
       final responsio = await dio.get('https://pokeapi.co/api/v2/pokemon/$pokemonId');
+
+      final pokemon = PokemonMapper.pokeApiPokemonToEntity(responsio.data);
       
+      return (pokemon, 'Data obtenida correctamente');
+
     } catch (e) {
 
+      return (null, 'No se pudo obtenter el Pokemon');
     }
 
   }
